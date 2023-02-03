cc9b1c3a686ea3fd9abd403ff88b13aa6a0a1cc5
Ahmed <a.ehab@seamlabs.com>
2023-02-03 15:38:21 +0200

    disableHeroko

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/app'));
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/app/'}),
+// );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
